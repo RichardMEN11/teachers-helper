48b07519f756ba7f5de975b07964db2f7d6aaadc
RichardMEN11 <richardmenning@googlemail.com>
2023-08-11 18:25:11 +0200

    Initial Commit

---
+++ teachers-helper/teachers_helperApp.swift
@@ -0,0 +1,20 @@
+//
+//  teachers_helperApp.swift
+//  teachers-helper
+//
+//  Created by Richard Menning on 11.08.23.
+//
+
+import SwiftUI
+
+@main
+struct teachers_helperApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
